b17ae67329581aca12352c924b0cdc27ee1d6fb1
DJsawada <satisfaction00000@gmail.com>
2016-10-02 10:27:55 +0900

    2kaihi

--- urg_library-1.2.0/samples/c/find_port.c
+++ urg_library-1.2.0/samples/c/find_port.c
@@ -7,18 +7,13 @@
 
 #include "urg_serial_utils.h"
 #include <stdio.h>
-
-
-int main(void)
-{
+int main(void){
     int found_port_size = urg_serial_find_port();
     int i;
-
     if (found_port_size == 0) {
         printf("could not found ports.\n");
         return 1;
     }
-
     for (i = 0; i < found_port_size; ++i) {
         printf("%s", urg_serial_port_name(i));
         if (urg_serial_is_urg_port(i)) {
@@ -26,6 +21,5 @@ int main(void)
         }
         printf("\n");
     }
-
     return 0;
 }
